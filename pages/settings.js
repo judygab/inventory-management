d10fc404957839caf78d870ef359e229686a559f
judygab <ivditigabeskiria@gmail.com>
2021-10-26 22:37:37 -0400

    updated logo

--- pages/settings.js
+++ pages/settings.js
@@ -27,8 +27,10 @@ const SettingsPage = () => {
     const MenuItem = ({ title, desc, ...rest }) => {
       return (
         <Box p={5} shadow="md" borderWidth="1px" {...rest}>
-          <Heading fontSize="xl">{title}</Heading>
+        <button>
+          <Heading textAlign="left" fontSize="xl">{title}</Heading>
           <Text mt={4}>{desc}</Text>
+          </button>
         </Box>
       )
     }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    maxWidth: {
+     '1/4': '25%',
+     '1/2': '50%',
+     '3/4': '75%',
+   },
   },
   variants: {
     extend: {},
